7e840454c9a5a72ec6aa31352e652741501ec89c
Kai Kowalczyk <kai.kowalczyk@gmail.com>
2021-11-28 22:17:52 +0100

    zakup defined with decorator

--- accountant_lib.py
+++ accountant_lib.py
@@ -86,5 +86,54 @@ def sprzedaz(
                 store.write(line)    
         with open('logs.txt', 'a') as logs:
             logs.write(f'Sprzedano {amount} szt. towaru: {item_id}. Saldo bieżące: {acc_balance} '+ '\n')
+
+@manager.assign('zakup')
+def zakup(
+        manager, 
+        filename=sys.argv[1], 
+        item_id=sys.argv[2], 
+        price=sys.argv[3], 
+        amount=sys.argv[4]
+        ):   
+
+    with open(filename) as store:
+        acc_balance = float(store.readline())
+        products = store.readlines()
+        purchase = False
+        whole_price = float(price) * float(amount)
+        item_found = False
+        for line in products:
+            if item_id in line:
+                item_found = True
+                product_data = line.split(';')
+                product_id = product_data[0]
+                product_price = float(product_data[1])
+                product_amount = int(product_data[2])
+                if acc_balance >= whole_price:
+                    updated_amount = product_amount + int(amount)
+                    updated_data = f'{item_id};{product_price};{updated_amount}'
+                    i = products.index(line)
+                    products[i] = updated_data + '\n'
+                    acc_balance = float(acc_balance) - (float(amount) * float(price))
+                    purchase = True
+                else:
+                    print(f'Za mało środków na koncie! Saldo bieżące: {acc_balance} zł.')
+                break
+            else:
+                pass
+        if not item_found :
+            if acc_balance >= whole_price:
+                    products.append('\n' + f'{item_id};{float(price)};{amount}')
+                    acc_balance = float(acc_balance) - (float(amount) * float(price))
+                    purchase = True
+            else:
+                print(f'Za mało środków na koncie! Saldo bieżące: {acc_balance} zł.')        
+    if purchase:
+        with open(filename, 'w') as store:
+            store.write(str(acc_balance) + '\n') 
+            for line in products:
+                store.write(line)
+        with open('logs.txt', 'a') as logs:
+            logs.write(f'Zakupiono {amount} szt. towaru: {item_id}. Saldo bieżące: {acc_balance} '+ '\n')
     
 
